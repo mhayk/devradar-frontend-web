b11b8da5b41512d34ad72120d398908772f5ffad
Mhayk Whandson <mhayk@kaumediagroup.com>
2020-01-15 17:53:28 +0000

    Some modifications ... :}
    Signed-off-by: Mhayk Whandson <mhayk@kaumediagroup.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div id="app">
+    <aside>
+      <strong>Add</strong>
+      <form>
+        <label htmlFor="github_username_label">Github user</label>
+        <input name="github_username" id="github_username_label"/>
+      </form>
+    </aside>
+    <main></main>
+  </div>
+  )
 }
 
 export default App;
