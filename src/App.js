57332d7b79eca2df49020afe26f511c93a1bcb1e
Mhayk Whandson <hi@mhayk.com>
2020-01-16 11:52:02 +0000

    [code-refactoring] An component DevItem has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem'
+
 function App() {
   const [devs, setDevs] = useState([])
 
@@ -51,7 +53,7 @@ function App() {
     setGithubUsername('')
     setTechs('')
 
-    setDevs([...devs, response.data])
+    setDevs([...devs, response.data]);
   }
 
   return (
@@ -113,20 +115,9 @@ function App() {
       <ul>
         {
           devs.map( dev => (
-            <li key={dev._id} className="dev-item">
-              <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-                <div className="user-info">
-                  <strong>{dev.name}</strong>
-                  <span>{dev.techs.join(', ')}</span>
-                </div>
-              </header>
-              <p>{dev.bio}</p>
-              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
-            </li>
-          )
-        )
-      }
+            <DevItem key={dev._id} dev={dev} />
+          ))
+        }
       </ul>
     </main>
   </div>
