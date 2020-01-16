bb5a023c5a687487443ff27a2e90707523d34067
Mhayk Whandson <hi@mhayk.com>
2020-01-16 11:04:09 +0000

    The devs is being fetched from the backend and listed.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
@@ -6,6 +7,8 @@ import './Sidebar.css';
 import './Main.css';
 
 function App() {
+  const [devs, setDevs] = useState([])
+
   const [github_username, setGithubUsername] = useState('')
   const [techs, setTechs] = useState('')
   const [latitude, setLatitude] = useState('')
@@ -25,8 +28,30 @@ function App() {
     )
   }, []);
 
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data)
+    }
+
+    loadDevs()
+  }, [])
+
   async function handleAddDev(e) {
     e.preventDefault()
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    })
+
+    setGithubUsername('')
+    setTechs('')
+
+    setDevs([...devs, response.data])
   }
 
   return (
@@ -86,61 +111,22 @@ function App() {
 
     <main>
       <ul>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/1500873?s=460&v=4" alt="Mhayk Whandson" />
-            <div className="user-info">
-              <strong>Mhayk Whandson</strong>
-              <span>ReactJS, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>Passionate about JavaScript, ReactJS, React Native, NodeJS and the entire ecosystem around these technologies.</p>
-          <a href="https://github.com/mhayk">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/1500873?s=460&v=4" alt="Mhayk Whandson" />
-            <div className="user-info">
-              <strong>Mhayk Whandson</strong>
-              <span>ReactJS, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>Passionate about JavaScript, ReactJS, React Native, NodeJS and the entire ecosystem around these technologies.</p>
-          <a href="https://github.com/mhayk">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/1500873?s=460&v=4" alt="Mhayk Whandson" />
-            <div className="user-info">
-              <strong>Mhayk Whandson</strong>
-              <span>ReactJS, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>Passionate about JavaScript, ReactJS, React Native, NodeJS and the entire ecosystem around these technologies.</p>
-          <a href="https://github.com/mhayk">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/1500873?s=460&v=4" alt="Mhayk Whandson" />
-            <div className="user-info">
-              <strong>Mhayk Whandson</strong>
-              <span>ReactJS, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>Passionate about JavaScript, ReactJS, React Native, NodeJS and the entire ecosystem around these technologies.</p>
-          <a href="https://github.com/mhayk">Acessar perfil no Github</a>
-        </li>
-        <li className="dev-item">
-          <header>
-            <img src="https://avatars2.githubusercontent.com/u/1500873?s=460&v=4" alt="Mhayk Whandson" />
-            <div className="user-info">
-              <strong>Mhayk Whandson</strong>
-              <span>ReactJS, React Native, Node.js</span>
-            </div>
-          </header>
-          <p>Passionate about JavaScript, ReactJS, React Native, NodeJS and the entire ecosystem around these technologies.</p>
-          <a href="https://github.com/mhayk">Acessar perfil no Github</a>
-        </li>
+        {
+          devs.map( dev => (
+            <li key={dev._id} className="dev-item">
+              <header>
+                <img src={dev.avatar_url} alt={dev.name} />
+                <div className="user-info">
+                  <strong>{dev.name}</strong>
+                  <span>{dev.techs.join(', ')}</span>
+                </div>
+              </header>
+              <p>{dev.bio}</p>
+              <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+            </li>
+          )
+        )
+      }
       </ul>
     </main>
   </div>
