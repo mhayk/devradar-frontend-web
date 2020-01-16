5be4ed13282994bcafae4a515b1f7100dc169f74
Mhayk Whandson <mhayk@kaumediagroup.com>
2020-01-16 09:12:18 +0000

    aside bar style has been finished.
    Signed-off-by: Mhayk Whandson <mhayk@kaumediagroup.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './global.css';
 import './App.css';
+import './Sidebar.css';
 
 function App() {
   return (
@@ -9,8 +10,27 @@ function App() {
     <aside>
       <strong>Add</strong>
       <form>
-        <label htmlFor="github_username_label">Github user</label>
-        <input name="github_username" id="github_username_label"/>
+        <div className="input-block">
+          <label htmlFor="github_username">Github user</label>
+          <input name="github_username" id="username_github" required/>
+        </div>
+
+        <div className="input-block">
+          <label htmlFor="techs">Tecnologies</label>
+          <input name="techs" id="techs" required/>
+        </div>
+
+        <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input name="latitude" id="latitude" required/>
+        </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input name="longitude" id="longitude" required/>
+        </div>
+        </div>
+        <button type="submit">Save</button>
       </form>
     </aside>
     <main></main>
